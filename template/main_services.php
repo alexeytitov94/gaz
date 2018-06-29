6ee199811eb74e3f03b337b6c18b27f36db94fe5
alexeytitov94 <alexeytitov94@mail.ru>
2018-06-29 12:03:39 +0300

    update links

--- template/main_services.php
+++ template/main_services.php
@@ -1,10 +1,10 @@
 <div class="col-lg-3 menu_service" style="height: 100%">
     <ul class="list-group services_list">
-        <li>Газификация</li>
-        <li>Промышленная автономная газификация</li>
-        <li>Монтаж газопроводов и котельныхы</li>
-        <li>Обслуживание и ремонт</li>
-        <li>Заправка газом</li>
+        <li><a class="link_menu" href="http://alexeytito.beget.tech/gaz/service_auto_gaz_home_1.php">Газификация</a></li>
+        <li><a class="link_menu" href="http://alexeytito.beget.tech/gaz/service_auto_gaz_company_2.php">Промышленная автономная газификация</a></li>
+        <li><a class="link_menu" href="http://alexeytito.beget.tech/gaz/service_gaz_tchasty_dom_3.php">Монтаж</a></li>
+        <li><a class="link_menu" href="http://alexeytito.beget.tech/gaz/service_gaz_and_kotelin_4.php">Обслуживание и ремонт</a></li>
+        <li><a class="link_menu" href="http://alexeytito.beget.tech/gaz/gaz_delivery_5.php">Заправка газом</a></li>
     </ul>
     <div class="gaz_pasport">
         <h6>Устанавливаем только проверенные газгольдеры </h6>
